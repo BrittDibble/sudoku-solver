47e5ce9ce7d78b5407fb0b59aaf8408413ef7ffc
Brittany Dibble <dibbl001@cougars.csusm.edu>
2015-05-20 17:01:01 -0700

    Client, printBoard, & constructor update
    Created client.cpp
    Made it so that the constructor also fills in the pencil marks array.
    Deleted the mess of code under printBoard as it was hard to read and did
    not work.

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -10,6 +10,10 @@ SudokuSolver::SudokuSolver()
 		for(int j = 0; j < 9; j++)
 		{
 			gameBoard[i][j].elem = 0;
+			for(int q = 0; q < 9;q++)
+			{
+				gameBoard[i][j].pencil[q] = q+1;
+			}
 		}
 	}
 }
@@ -25,110 +29,5 @@ void SudokuSolver::enterNumber(int x, int y, int number)
 //
 void SudokuSolver::printBoard()
 {
-	bool noPencilOne = true;
-	bool noPencilTwo = true;
-	bool noPencilThree = true;
-	cout << "= === = === = === =\n"
-	for(int j = 0; j < 9; j ++)
-	{
-		for(int i = 0; i < 9; i++)
-		{
-			cout << "= "
-			if(gameBoard[i][j].elem != 0)
-			{
-				cout << gameBoard[i][j].elem;
-				cout << gameBoard[i][j].elem;
-				cout << gameBoard[i][j].elem;
-				cout << " ";
-			}
-			else
-			{
-				if(i == 0 || i == 1 || i == 2)
-				{
-					if(!(gameBoard[i][j].pencil[0] != 0 || gameBoard[i][j].pencil[0] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[0];
-					}
-					else
-					{
-						cout << " ";
-					}
-					if(!(gameBoard[i][j].pencil[1] != 0 || gameBoard[i][j].pencil[1] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[1];
-					}
-					else
-					{
-						cout << " ";
-					}
-					if(!(gameBoard[i][j].pencil[2] != 0 || gameBoard[i][j].pencil[2] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[2];
-						cout << " ";
-					}
-					else
-					{
-						cout << "  ";
-					}
-				}
-				else if(i == 3 || i == 4 || i == 5)
-				{
-					if(!(gameBoard[i][j].pencil[3] != 0 || gameBoard[i][j].pencil[3] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[3];
-					}
-					else
-					{
-						cout << " ";
-					}
-					if(!(gameBoard[i][j].pencil[4] != 0 || gameBoard[i][j].pencil[4] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[4];
-					}
-					else
-					{
-						cout << " ";
-					}
-					if(!(gameBoard[i][j].pencil[5] != 0 || gameBoard[i][j].pencil[5] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[5];
-						cout << " ";
-					}
-					else
-					{
-						cout << "  ";
-					}
-				}
-				else
-				{
-					if(!(gameBoard[i][j].pencil[6] != 0 || gameBoard[i][j].pencil[6] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[6];
-					}
-					else
-					{
-						cout << " ";
-					}
-					if(!(gameBoard[i][j].pencil[7] != 0 || gameBoard[i][j].pencil[7] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[7];
-					}
-					else
-					{
-						cout << " ";
-					}
-					if(!(gameBoard[i][j].pencil[8] != 0 || gameBoard[i][j].pencil[8] != NULL))
-					{
-						cout << gameBoard[i][j].pencil[8];
-						cout << " ";
-					}
-					else
-					{
-						cout << "  ";
-					}
-				}
-			}
-		}
-		cout << "=\n"
-	}
+	cout << "This will be the board once it is done";
 }
