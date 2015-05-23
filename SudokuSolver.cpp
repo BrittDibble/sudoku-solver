d4f6516479045f371959ea84add3b1b6aaee4526
Brittany Dibble <dibbl001@cougars.csusm.edu>
2015-05-22 19:33:21 -0700

    Fixed Display to work with the 1D Array
    printBoard works now for showing pencil marks.

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -29,10 +29,48 @@ How to Call:Give the row, column  and the number.
 void SudokuSolver::enterNumber(int x, int y, int number)
 {
 	//I do nothing right now
+	return;
+}
+/* Purpose: To be used by the printBoard. Prints out the row of the board including pencil marks.
+How to Call: Provide the row number from 0 to 26;
+*/
+void SudokuSolver::printRow(int rowNumber)
+{
+	int row = rowNumber / 3;
+	cout << "=|";
+	for(int i = 0; i < 9; i++)
+	{
+		if(i % 3 == 0)
+		{
+			cout << "|";
+		}
+		cout << gameBoard[row].pencil[rowNumber*3]
+		<< gameBoard[row].pencil[rowNumber*3 + 1]
+		<< gameBoard[row].pencil[rowNumber*3 + 2]
+		<< "|";
+	}
+	cout << "=";
+	return;
 }
 /* Purpose: Prints out the gameBoard and the pencil marks in a readable format. 
 */
 void SudokuSolver::printBoard()
 {
-	//I do nothing right now
+	string aString = "=|======================================|=\n";
+	for(int i = 0; i < 27; i++)
+	{
+		if(i % 3 == 0)
+		{
+			cout << aString;
+		}
+		if(i % 9 == 0)
+		{
+			cout << aString;
+		}
+		printRow(i);
+		cout << "\n";
+	}
+	cout << aString;
+	cout << aString;
+	return;
 }
